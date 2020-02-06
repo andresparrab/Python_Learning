69072c9fa5dffcdce131c3658315430a69c78bf2
andresparrab <andres.parrab@gmail.com>
2020-02-06 19:10:43 +0100

    update4

---
+++ anti_vowel.py
@@ -0,0 +1,14 @@
+def anti_vowel(text):
+  vowel='aeiouAEIOU'
+# create a list that will hold the new characters
+  result =[]
+  # loop trough the text
+  for char in text:
+      # if the letter is not found in vowesl='aeiouAeiou' the add the character to the list result.
+      if char not in vowel:
+          result.append(char)
+          # return the letter joined together as a string
+  return ''.join(result)
+
+
+print(anti_vowel('AndRes'))
