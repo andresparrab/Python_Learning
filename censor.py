5edf8754ace249f7763d7ea11c485df2a84b984b
andresparrab <andres.parrab@gmail.com>
2020-02-07 22:08:48 +0100

    update4

---
+++ censor.py
@@ -0,0 +1,13 @@
+def censor(text,word):
+    for letter in text:
+ #       if word in text:
+ #           for letter in word:
+ #               word=word.replace(letter,'*')
+ #    #           print(word)
+ #       else:
+
+
+    return word
+
+
+print(censor('this hack is wack','hack'))
