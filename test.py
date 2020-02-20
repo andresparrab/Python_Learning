65f41e1bdd8df12a5ce841ecfb32965f19e2cf19
andresparrab <andres.parrab@gmail.com>
2020-02-20 09:45:01 +0100

    test code

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 
+import sys
+
+print(len(sys.argv))
+
+print(sys.argv)
+
+name = input('Enter your name')
+age = input('enter you age')
+
+print('welcome %s'%(name))
+print('your age is : %d'%(int(age)))
+
+print('After five year you age woud be: %2d' %(int(age)+5))
