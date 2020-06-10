15818caab3bdec0e15c3c250f5d391016b886db9
doctor <andres.parrab@gmail.com>
2020-06-10 16:09:50 +0200

    sockest listening and bindning

--- Sokets/04  Sockets Binding and listening.py
+++ Sokets/04  Sockets Binding and listening.py
@@ -1,1 +1,18 @@
-sockets
+import socket
+import sys
+
+host = ''
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((host,port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(5)
+
+conn,addr = s.accept()
+
+print('connected: ' +addr[0]+':'+str(addr[1]))
